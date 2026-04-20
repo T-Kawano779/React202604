7b531f2ba13aede2baac837d7bb5d19480fe9b6e
T-Kawano779 <kawano@unity.with.co.jp>
2026-04-20 09:23:02 +0900

    Reactプロジェクトを追加

---
+++ src/App.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+describe('App', () => {
+  it('Vite + React の見出しが表示される', () => {
+    render(<App />)
+
+    const heading = screen.getByText('Vite + React')
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('count ボタンが表示される', () => {
+    render(<App />)
+    expect(screen.getByRole('button')).toBeInTheDocument()
+  })
+
+})
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button>count is 0</button>
+    </>
+  )
+}
+
+export default App
